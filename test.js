00124467cfcd48f4663676409c93db40e0fa2805
Sindre Sorhus <sindresorhus@gmail.com>
2016-10-20 01:40:44 +0700

    [breaking] require Node.js 4 and change second argument
    The second argument was changed from a boolean to an options object.

--- test.js
+++ test.js
@@ -1,23 +1,16 @@
-/* eslint-disable arrow-body-style */
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test('call function once', t => {
 	let i = 0;
-	const f = fn(() => i++);
+	const f = m(() => i++);
 	t.is(f(), 0);
 	t.is(f(), 0);
 	t.is(f(), 0);
 });
 
 test('option to throw is called more than once', t => {
-	const f = fn(() => {}, true);
+	const f = m(() => {}, {throw: true});
 	f();
 	t.throws(f, Error);
-});
-
-test('use the name of the function if available', t => {
-	const f = fn(function foo() {}, true);
-	f();
-	t.throws(f, 'foo() can only be called once.');
 });
